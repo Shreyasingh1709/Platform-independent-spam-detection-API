da6d4fdb1e45f3528f2b46e3e7b94dfd5e3e43fe
Shreyasingh1709 <shreyakumarisingh0511@gmail.com>
2026-04-14 16:38:49 +0530

    all the changes pushed

--- distilbert.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-# Load pretrained model
-classifier = pipeline("text-classification", model="distilbert-base-uncased")
-
-# Test example
-result = classifier("You have won free money!")
-print(result)
